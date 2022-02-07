3cd7de35328b55255a3c7ec43cd9e3277d9fd8b9
DevOjr <deoliveirajr1@gmail.com>
2022-02-07 14:21:00 -0300

    exercicio 1 e 2

---
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D1-primeiros_passos/script.js
@@ -0,0 +1,36 @@
+//1.
+let a = 2;
+let b = 7;
+
+//adicao
+let adicao = (a+b);
+console.log(adicao);
+
+//subtracao
+let subtracao = (a-b);
+console.log(subtracao);
+
+//multiplicacao
+let mult = (a*b);
+console.log(mult);
+
+//divisao
+let div = (a/b);
+console.log(div);
+
+//modulo
+let mod = (a%b);
+console.log(mod);
+
+//2.
+
+const a = 2;
+const b = 7;
+
+if(a>b){
+    console.log(a)
+} else if(b>a) {
+    console.log(b)
+} else {
+    console.log("numeros sao equivalentes")
+}

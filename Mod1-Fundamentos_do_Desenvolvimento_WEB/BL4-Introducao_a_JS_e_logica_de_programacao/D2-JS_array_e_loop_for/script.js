a47767b0debc5fae56d18e590984b71d097f5f5e
DevOjr <deoliveirajr1@gmail.com>
2022-02-09 13:17:52 -0300

    resoluçao de exercicios bonus

--- Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D2-JS_array_e_loop_for/script.js
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D2-JS_array_e_loop_for/script.js
@@ -78,3 +78,44 @@ for (let i = 0; i < arrayList.length; i += 1 ) {
     listaDivisao.push(arrayList[i] / 2)
 }
     console.log(listaDivisao);
+
+
+//Bonus-1
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] < array[secondIndex]) {
+        let position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;    }
+    }
+  }
+
+  console.log("ordem crescente: " + array)
+
+//Bonus-2
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] > array[secondIndex]) {
+        let position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;    }
+    }
+  }
+
+  console.log("ordem decrescente: " + array)
+
+  //Bonus-3
+  let array2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let newArray = []
+  
+  for(i = 0; i < array2.length; i += 1) {
+  if( i + 1 < array2.length) {
+     newArray.push(array2[i] * array2[i+1])
+  } else { 
+      newArray.push(array2[i] * 2)
+  }
+  
+}
+  console.log("New array: " + newArray);

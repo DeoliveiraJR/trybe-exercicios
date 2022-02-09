de876861fcc07b054627d31e3e53f9c5f9c4d3a6
DevOjr <deoliveirajr1@gmail.com>
2022-02-09 18:51:24 -0300

    fechamento exercicios - dia 3 | Bloco 4 | Modulo 1

--- Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D3-logica_de_programacao_e_algoritmos/script.js
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D3-logica_de_programacao_e_algoritmos/script.js
@@ -16,3 +16,53 @@ for (let index = 10; index > 0; index -= 1) {
 console.log("fatorial total: " + fatorial);
 
 //2.
+let word = 'angra';
+let wordInvert = '';
+
+for (let index = 0; index < word.length; index += 1) {
+    wordInvert += word[word.length-1-index];
+}
+
+console.log(wordInvert)
+
+//3.
+let arrayText = ['java', 'javascript', 'python', 'html', 'css'];
+let biggestWord = arrayText[0];
+
+for (let i = 0; i < arrayText.length-1; i += 1 ) {
+    if(arrayText[i].length > biggestWord.length) {
+        biggestWord = arrayText[i]
+    }
+}
+console.log("A maior palavra da lista: " + biggestWord)
+console.log("total de letras: " + biggestWord.length)
+
+//4.
+let smallestWord = arrayText[0]
+
+for (let i = 0; i < arrayText.length-1; i += 1 ) {
+    if(arrayText[i].length < biggestWord.length) {
+        biggestWord = arrayText[i]
+    }
+}
+console.log("A menor palavra da lista: " + smallestWord)
+console.log("total de letras: " + smallestWord.length)
+
+//5.
+
+maiorNumeroPrimo = 0;
+
+for(let numerador = 1; numerador <= 50; numerador += 1) {
+    ehprimo = true;
+    for(let divisor = 2; divisor < numerador; divisor += 1) {
+        if(numerador % divisor === 0) { 
+            ehprimo = false;
+        }
+    }   
+    
+    if (ehprimo) {
+        maiorNumeroPrimo = numerador;
+    }
+}
+
+console.log("Maior numero primo: " + maiorNumeroPrimo)

593756648af7bfebe81cdb3c8f30c77ba8260c17
DevOjr <deoliveirajr1@gmail.com>
2022-02-10 16:36:20 -0300

    exercicio dia 4 - BL 4 - Mod 1

--- Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D4-objetos_e_funcoes/script.js
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL4-Introducao_a_JS_e_logica_de_programacao/D4-objetos_e_funcoes/script.js
@@ -18,6 +18,7 @@ console.log("A jogadora " + player.name + " " + player.lastname + " foi eleita a
 
 console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata")
 
+//Parte 1
 //1.
 let info = {
     personagem: 'Margarida',
@@ -85,3 +86,84 @@ let leitor = {
 
   //8)
   console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
+
+//Parte 2
+//1)
+function palindromo(word) {
+    for( i = 0; i < palavra.length-1; i += 1) {
+      newWOrd = word(word.length - (i))
+    }
+    return console.log(newWOrd)
+  }
+  
+//2)
+
+function arraylist(numbers) {
+  let sum = 0;
+
+  for(let i in numbers) {
+    if(numbers[sum] < numbers[i]) {
+      sum = i;
+    }
+  }
+
+  return sum;
+}
+
+console.log("2) indice correspondente = " + arraylist([2, 3, 6, 7, 10, 1]))
+
+//3.
+function arraylist(numbers) {
+  let sum = 0;
+
+  for(let i in numbers) {
+    if(numbers[sum] > numbers[i]) {
+      sum = i;
+    }
+  }
+
+  return sum;
+}
+
+console.log("3) indice correspondente = " + arraylist([2, 4, 6, 7, 10, 0, -3]))
+
+//4.
+function arraylist(names) {
+  let maiorNome = names[0];
+ 
+  /*console.log(maiorNome.length);
+  console.log(names[4].length);
+  console.log(names[4]);*/
+  
+    for(let i in names) {
+    if(maiorNome.length < names[i].length) {
+      maiorNome = names[i];
+    }
+    
+  }
+    return maiorNome;
+}
+    
+
+
+console.log("4) Maior nome: " + arraylist(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+//5.
+function arraylist(names) {
+  let maiorNome = names[0];
+ 
+  /*console.log(maiorNome.length);
+  console.log(names[4].length);
+  console.log(names[4]);*/
+  
+    for(let i in names) {
+    if(maiorNome.length < names[i].length) {
+      maiorNome = names[i];
+    }
+    
+  }
+    return maiorNome;
+}
+    
+
+console.log("4) Maior nome: " + arraylist(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));

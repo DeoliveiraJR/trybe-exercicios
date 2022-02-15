98f8d4a00e3e4132e8336b939dbb921a07c3cfc6
DevOjr <deoliveirajr1@gmail.com>
2022-02-14 22:47:17 -0300

    Q1) exercicio Dia 2 | BL 5 | Mod-1

---
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL5-JavaScript_Dom_elementos_e_web_storage/D2-trabalhando_com_elementos/script.js
@@ -0,0 +1,54 @@
+/*document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'blue'
+
+texto1 = document.querySelector('#primeiroFilhoDoFilho');
+texto1.innerText = "3) Exercicio de fixacao - req 3";
+
+4)document.querySelector('#pai').firstElementChild;
+
+5)document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+
+6)document.querySelector('#elementoOndeVoceEsta').nextSibling;
+
+7)document.querySelector('#elementoOndeVoceEsta').nextElementSibling
+
+8)document.querySelector('#pai').lastChild.previousElementSibling*/
+
+/*function task1() {
+
+let var1 = document.querySelector('#pai');
+let var2 = document.createElement('section');
+var2.innerText = "deu certo";
+return var1.appendChild(var2);
+}
+
+task1();
+
+function task2() {
+
+    let var3 = document.querySelector('#elementoOndeVoceEsta');
+    let var4 = document.createElement('section');
+    var4.innerText = "deu certo de novo";
+    return var3.appendChild(var4);
+}
+    
+task2();
+
+function task3() {
+
+    let var5 = document.querySelector('#primeiroFilhoDoFilho');
+    let var6 = document.createElement('section');
+    var6.innerText = "E de novo, e de novo";
+    return var5.appendChild(var6);
+}
+    
+task3();*/
+
+function q1() {
+
+    let var1 = document.querySelector('body');
+    let var2 = document.createElement('h1');
+    var2.innerText = "Exercício 5.2 - JavaScript DOM";
+    return var1.appendChild(var2);
+    }
+    
+    q1();

831d6ebe9b75dd29e14128e5241a6b30e56bf296
DevOjr <deoliveirajr1@gmail.com>
2022-03-18 14:44:10 -0300

    update de repositorio

---
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL8-higher_order_functions/D2-forEach_find_some_every_sort/script.js
@@ -0,0 +1,170 @@
+//#8.1 - forEach:
+
+//Exemplo que retorna uma função que converte todas as palavavras em maiusculas :
+
+/* const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (array, index) => {
+  names[index] = array.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names) */
+
+//PARA FIXAR: 
+//1 - Use o método forEach chamando a callback showEmailList para apresentar os emails :
+
+/* const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  }; */
+  
+  // Adicione seu código aqui
+  
+/*   emailListInData.forEach(showEmailList);
+  console.log(emailListInData); */
+
+//#8.1 - find:
+
+//exemplo - retorna um elemento PAR dentro de um array e armazena em uma variável:
+/* const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven);
+
+console.log(verifyEven(9));
+console.log(verifyEven(14));
+ */
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+
+/*const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); */
+
+//PARA FIXAR: 
+//1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista :
+/* const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(findDivisibleBy3And5) */
+
+//2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+/* const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = names.find((word) => word.length === 5);
+
+console.log(findNameWithFiveLetters); */
+
+//3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+
+/* const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+  
+  return musicas.find((array) => array.id === id);
+  
+  }
+
+  console.log(findMusic('31031685')) */
+
+//#8.2 - Some e Every:
+//Exemplo(some) - testa se a primeira letra de cada string do array começa com uma letra passada: 
+/* const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false */
+
+//Exemplo(every) - testa se a condição aprovado é true em todos elementos do objeto:
+
+/* const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = (studentGrades) => (
+  Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+);
+
+console.log(verifyGrades(grades)); */
+
+//PARA FIXAR: 
+// 1. Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+/* const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  result = arr.some((element) => element === name)
+  return result;
+}
+
+console.log(hasName(names, 'JR')) */
+
+/* 2. Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso
+ contrário false , use every ; */
+
+/*  const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((element) => element.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 20)); */
+
+//#8.2 - sort:
+//Exemplo - ordenando stings:
+
+/* const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food); */
+
+//Exemplo - ordenando numeros(ordem crescente):
+
+/* const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points);  */
+
+//PARA FIXAR: 
+// 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente:
+
+/* const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age - b.age);
+
+console.log(people); */
+
+// 2. Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente:
+
+/* people.sort((a, b) => b.age - a.age);
+
+console.log(people); */

4695daf69dfcb8524a09ee82d715e99e9471090c
DevOjr <deoliveirajr1@gmail.com>
2022-03-19 18:42:59 -0300

    update repositorio 8.4

--- Mod1-Fundamentos_do_Desenvolvimento_WEB/BL8-higher_order_functions/D4-reduce/script.js
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL8-higher_order_functions/D4-reduce/script.js
@@ -1,3 +1,53 @@
 // #8.4 - reduce:
 
-// Exemplo :
+// Exemplo 1: o resultado da soma acumulada de uma lista de arrays: 
+
+/* const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const sumNumbers = numbers.reduce((result, number) => result + number)
+console.log(sumNumbers); */
+
+// Exemplo 2: retorna o maior numero de uma lista de array:
+
+/* const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => (bigger > number) ? bigger : number;
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger) */
+
+// Exemplo 3: faça uma função que some todos os números pares do array:
+//Solução usando filter + reduce:
+
+/* const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (currentValue, number) => currentValue + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair);
+
+console.log(sumNumbers(numbers)); */
+
+//solução somente com reduce:
+
+/* const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPair = (currentValue, number) => (
+  (number % 2 === 0) ? currentValue + number : currentValue
+);
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+console.log(sumNumbers(numbers)); */
+
+/* const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+
+console.log(flatten()); */

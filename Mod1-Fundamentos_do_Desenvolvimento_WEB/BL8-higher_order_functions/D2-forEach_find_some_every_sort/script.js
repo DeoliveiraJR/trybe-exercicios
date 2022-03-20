2edd8a2aeec64dd96cdaf5a4507260ccb3beb8a9
DevOjr <deoliveirajr1@gmail.com>
2022-03-20 16:57:29 -0300

    exercicio 8.2

--- Mod1-Fundamentos_do_Desenvolvimento_WEB/BL8-higher_order_functions/D2-forEach_find_some_every_sort/script.js
+++ Mod1-Fundamentos_do_Desenvolvimento_WEB/BL8-higher_order_functions/D2-forEach_find_some_every_sort/script.js
@@ -1,4 +1,4 @@
-//#8.1 - forEach:
+//#8.2 - forEach:
 
 //Exemplo que retorna uma função que converte todas as palavavras em maiusculas :
 
@@ -30,7 +30,7 @@ console.log(names) */
 /*   emailListInData.forEach(showEmailList);
   console.log(emailListInData); */
 
-//#8.1 - find:
+//#8.2 - find:
 
 //exemplo - retorna um elemento PAR dentro de um array e armazena em uma variável:
 /* const numbers = [19, 21, 30, 3, 45, 22, 15];
@@ -168,3 +168,155 @@ console.log(people); */
 /* people.sort((a, b) => b.age - a.age);
 
 console.log(people); */
+
+// EXERCICIOS 8.2:
+
+/* Estes exercícios praticam os conceitos de Higher Order Functions associados a outros já vistos, como arrow functions , template literals ,
+objetos e temas dos fundamentos. Essa mistura de conceitos é muito importante para seu aprendizado, então use tudo o que sabe para resolver
+os exercícios!
+
+Utilize seguinte código como template para realizar os exercícios: */
+
+/* const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+]; */
+
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano  .
+
+// usando o find:
+
+/* const firstAuthor = books.find((books) => {
+  return books.author.birthYear === 1947;
+}).author.name
+
+console.log(firstAuthor);
+ */
+//2 - Retorne o nome do livro de menor nome.
+//Dica: use a função forEach .
+
+/* const smallerName = () => {
+  let nameBook;
+
+  books.forEach((element) => {
+    if (!nameBook || element.name.length < nameBook.length) {
+     nameBook = element.name;
+    };
+  });
+  return nameBook;
+}
+
+console.log(smallerName()); */
+
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+/* const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((element) => element.name.length === 26);
+}
+
+console.log(getNamedBook()); */
+
+// 4 - Ordene os livros por data de lançamento em ordem decrescente:
+
+/* function booksOrderedByReleaseYearDesc() {
+  // escreva aqui seu código
+  return books.sort((a, b) => b.releaseYear - a.releaseYear);
+}
+
+console.log(booksOrderedByReleaseYearDesc()) */
+
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário:
+
+/* function everyoneWasBornOnSecXX() {
+ return books.every((element) => element.author.birthYear > 1900 && element.author.birthYear < 2000)
+}
+
+console.log(everyoneWasBornOnSecXX()) */
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+/* function someBookWasReleaseOnThe80s() {
+  return books.some((element) => element.releaseYear >= 1980 && element.releaseYear < 1990);
+}
+
+console.log(someBookWasReleaseOnThe80s()) */
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário:
+
+/* function authorUnique() {
+  return books.every((book) =>
+    !books.some((bookSome) => 
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
+}
+
+console.log(authorUnique()) */
